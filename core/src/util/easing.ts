5715c2642fe345d625c6d325f0fbd14133b37aef
xiaoxunyao <huchengyea@163.com>
2022-07-02 14:58:47 +0800

    fix: update easing

--- core/src/util/easing.ts
+++ core/src/util/easing.ts
@@ -113,3 +113,48 @@ export function easeInOutQuintfunction(t:number, b:number, c:number, d:number):n
   t -= 2;
   return c/2*(t*t*t*t*t + 2) + b;
 }
+
+export function easeInSine(t:number, b:number, c:number, d:number):number {
+  return -c * Math.cos(t/d * (Math.PI/2)) + c + b;
+}
+
+export function easeOutSine(t:number, b:number, c:number, d:number):number {
+  return c * Math.sin(t/d * (Math.PI/2)) + b;
+}
+
+export function easeInOutSine(t:number, b:number, c:number, d:number):number {
+  return -c/2 * (Math.cos(Math.PI*t/d) - 1) + b;
+}
+
+export function easeInExpo(t:number, b:number, c:number, d:number):number {
+  return c * Math.pow( 2, 10 * (t/d - 1) ) + b;
+}
+
+export function easeOutExpo(t:number, b:number, c:number, d:number):number {
+  return c * ( -Math.pow( 2, -10 * t/d ) + 1 ) + b;
+}
+
+export function easeInOutExpo(t:number, b:number, c:number, d:number):number {
+  t /= d/2;
+  if (t < 1) return c/2 * Math.pow( 2, 10 * (t - 1) ) + b;
+  t--;
+  return c/2 * ( -Math.pow( 2, -10 * t) + 2 ) + b;
+}
+
+export function easeInCirc(t:number, b:number, c:number, d:number):number {
+  t /= d;
+  return -c * (Math.sqrt(1 - t*t) - 1) + b;
+}
+
+export function easeOutCirc(t:number, b:number, c:number, d:number):number {
+  t /= d;
+  t--;
+  return c * Math.sqrt(1 - t*t) + b;
+}
+
+export function easeInOutCirc(t:number, b:number, c:number, d:number):number {
+  t /= d/2;
+  if (t < 1) return -c/2 * (Math.sqrt(1 - t*t) - 1) + b;
+  t -= 2;
+  return c/2 * (Math.sqrt(1 - t*t) + 1) + b;
+}
