2fbca9fa8302dbe4d3841b975b13792e0593c72b
xiaoxunyao <huchengyea@163.com>
2022-07-03 22:27:36 +0800

    fix: update example

--- example/src/pages/avatar/avatar.vue
+++ example/src/pages/avatar/avatar.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
-
+import {pickAvatar} from '@elonehoo/pick'
 </script>
 
 <template>
-  <div>1</div>
+  <div>
+    <pick-avatar text="Elone Hoo"/>
+    <pick-avatar text="Elone"/>
+  </div>
 </template>
 
 <style scoped>
