87c19bad8cc202515119f1eaad23e78e777f7597
xiaoxunyao <huchengyea@163.com>
2022-07-16 17:18:44 +0800

    exmaple: init images

--- example/src/pages/image/image.vue
+++ example/src/pages/image/image.vue
@@ -1,13 +1,20 @@
 <script setup lang="ts">
-import { pickImage } from '@elonehoo/pick'
+import { pickImage,pickImages } from '@elonehoo/pick'
 </script>
 
 <template>
-  <div>
-    <pick-image src="https://github.com/elonehoo.png" />
+  <div class="con-example-images">
+    <pick-images>
+      <pick-image :key="index" :src="`https://picsum.photos/400/400?image=3${index}`" v-for="(image, index) in 9" :alter="image" />
+      <pick-image :key="index" :src="`https://picsum.photos/400/400?image=4${index}`" v-for="(image, index) in 7" :alter="image" />
+    </pick-images>
   </div>
 </template>
 
 <style>
+.con-example-images {
+  max-height: 500px;
+  overflow: auto;
+}
 
 </style>
